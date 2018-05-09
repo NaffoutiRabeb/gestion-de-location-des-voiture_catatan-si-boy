f99958f0a37ee9b24cf47f2882aeec343c788b41
Naffoutirabeb <naffoutirabeb@gmail.com>
2018-05-09 15:10:23 +0100

    2 neveu fihier

--- footer.php
+++ footer.php
@@ -1,3 +1,26 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Catatan Si Boy a Bootstarp  Website Template | Home :: w3layouts</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<!--<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />-->
+<link href="<?php bloginfo('template_directory'); ?>/css/style.css"
+rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,800,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+<script src="js/jquery.min.js"></script>				
+</head>
+<body>
 <!-- footer -->
 	<div class="footer">
 		<div class="container">
@@ -11,4 +34,7 @@
 			</div>
 		</div>
 	</div>
+	<?php wp_footer(); ?>
 <!-- footer -->
+</body>
+</html>
