67634e48fa5b7e884d54f3214f3bc0d51e9164d0
jlassihaifa <36994787+jlassihaifa@users.noreply.github.com>
2018-05-11 11:31:41 +0200

    single

---
+++ single.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<div class="row">
+<div class="col-sm-12"> <?php if ( have_posts() ) : while ( have_posts()
+) : the_post(); get_template_part( 'content-single', get_post_format() );
+endwhile; endif; ?>
+</div> <!-- /.col -->
+</div> <!-- /.row -->
+<?php get_footer(); ?>
+<div class="blog-post">
+<h2 class="blog-post-title"><?php the_title(); ?></h2>
+<p class="blog-post-meta"><?php the_date(); ?> par <a href="#"><?php
+the_author(); ?></a></p>
+<?php get_contents(); ?>
+</div><!-- /.blog-post -->
+<h1>kjerhgj</h1>
