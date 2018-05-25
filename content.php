dcf8ff46a453619185521f77fef3e2f14b1ddea9
jlassihaifa <36994787+jlassihaifa@users.noreply.github.com>
2018-05-25 09:50:05 +0200

    new content

--- content.php
+++ content.php
@@ -4,55 +4,95 @@
 			<a href="single.html"></a>
 			<i class="aeroplane"> </i>
 	<div class="travelling">
+	<div class="haifa">
 			<div class="col-md-10 travelling1">
 				<h1><a href="single.html">
 				</a></h1>
 
-					<h1>location des voitures </h1>
-<?php the_title();?>
-<h1>bla bla bla....</h1>
+					<h1> location des voitures<basefont color="#000066"  face="Georgia, Times New Roman, Times, serif"  /></h1>
+
+
+
+
+
+
+<!--<?php the_title();?>-->
+
 <?php the_content();?>
-				<h5><a href="#">Samedi ,</a> 27 mais 2018 ,<a href="#"></a>maisson de location</h5>
-				<p> </p>
+				<h5><a href="#"></a> samedi 27 mais 2018 ,<a href="#"></a>maisson de location</h5>
+				<a href="index.html"><img src="rr.png" class="img-responsive" alt="" /></a>
 			</div>
 			<div class="col-md-1 travelling2">
-				<h3>catégorie des voiture</h3>
+				<h3><a>catégorie des voiture</a></h3>
+				
+				</div>
+				
+				
+				
+			
+				
+				<a href="index.html"><img src="img-sprite.png" class="img-responsive" alt="" /></a>
+				<img src="rr.png"  />
 				<p>
-<label for="your_fields[select]">menu des voiture </label>
-<br>
-<select name="your_fields[select]" id="your_fields[select]">
-<option value="option-one" <?php selected($meta['select'],'option-one'); ?>
->voiture classe</option>
-<option value="option-two" <?php selected($meta['select'],'optiontwo');?>>
-voiture cha3bia</option>
-</select>
-</p>
-				<p>
-<label for="your_fields[image]">Image Upload</label><br>
-<input type="text" name="your_fields[image]" id="your_fields[image]" class="meta-image
-regular-text" value="<?php echo $meta['image'];?>">
-<input type="button" class="button image-upload" value="Browse">
-</p>
+<label for="your_fields[select]"></label>
+
 <div class="image-preview"><img src="<?php echo $meta['image'];?>" style="max-width:
 250px;"></div>
-				<p>
-<label for="your_fields[checkbox]">ford
+				<div class="col-md-10 billgates1">
+<label for="your_fields[checkbox]"> <b><a href="#">categorie mini</a>
+			
 <input type="checkbox" name="your_fields[checkbox]" value="checkbox" <?php if(
 $meta['checkbox'] === 'checkbox') echo 'checked'; ?> >
-</label>
-<label for="your_fields[checkbox]">BMW
+</label>	</b><p>- plus petit catégorie de location de voiture .<br>-les véhicule sont les moins chers <br />-ne consommant que peu de carburant<br>-generalement equipee de mouteurs relativements puisante <br>-ce voiture sont adapte pour un court sejour ou de petit trajet de ville. </p>
+<label for="your_fields[checkbox]"><b><a href="#">categorie economique 
 <input type="checkbox" name="your_fields[checkbox]" value="checkbox" <?php if(
 $meta['checkbox'] === 'checkbox') echo 'checked'; ?> >
-</label>
-<label for="your_fields[checkbox]">Polo
+</label></a></b><p> -petit agil<br>-consement peu<br>- adapte pour deux personne avec bagage<br>-acceuilleur 4 personne pour des trajet plus court</p><br />
+<label for="your_fields[checkbox]"><b><a href="#">categorie lux
 <input type="checkbox" name="your_fields[checkbox]" value="checkbox" <?php if(
 $meta['checkbox'] === 'checkbox') echo 'checked'; ?> >
-</label>
-<label for="your_fields[checkbox]">306
+</label></a></b><p> -belle voiture parmis les modeles les plus prestigieux</p>
+
+<label for="your_fields[checkbox]"><a href="#"><b>categorie Monospace
 <input type="checkbox" name="your_fields[checkbox]" value="checkbox" <?php if(
 $meta['checkbox'] === 'checkbox') echo 'checked'; ?> >
-</label>
+</label></a></b><p>-ideal pour un voyage de groupe <br />-ideal pour un grand famille avec beaucoup de voyage
 </p>
+</div>
+			<div class="col-md-1 travelling2">
+				<h3><a>prix de voiture louée</a></h3>
+				
+				</div>
+<div class="cc">
+  	    <div class="container">
+			<div class="logo">
+				<a href="index.html"><img src="images/logo.png" class="img-responsive" alt="" /></a>
+			</div>	
+<br><label for="your_fields[text]">entreé le prix de voiture choisir</label>
+<input type="text" name="your_fields[text]" id="your_fields[text]" class="regular-text"
+value="<?php echo $meta['text']; ?>" >
+
+	<div class="lequotes">
+
+	<div class="clearfix"> 
+	<a href="#">commentaire</a></div>
+				<p>
+<label for="your_fields[textarea]"></label>
+<br>
+<textarea name="your_fields[textarea]" id="your_fields[textarea]" rows="5" cols="30"
+style="width:500px;"><?php echo $meta['textarea'];?></textarea>
+</p>
+		
+			<div class="col-md-12">
+				<a class="more" href="single.html"><input type="button"name="enregistre"  value="enregistre"/></a>
+			</div>
+					
+				
+<img src="images/rr.png" class="hh" alt="" /></a>
+					
+
+</p>
+
 				
 				
 			</div>
@@ -60,51 +100,31 @@ $meta['checkbox'] === 'checkbox') echo 'checked'; ?> >
 	</div>
 	<div class="lequotes">
 		
-			<div class="col-md-12">
-				<a class="more" href="single.html">lire plus</a>
-			</div>
+			
 			<div class="col-md-12 lequotes3 ">
 				<div class="lequotes1">
 					<img src="images/01.png" class="img-responsive" alt="" />
 				</div>
 				<div class="lequotes2">
-					<h3>prix de location des voiture</h3>
-					<p>
-<label for="your_fields[text]">entreé le prix de voiture choisir</label>
-<br>
-<input type="text" name="your_fields[text]" id="your_fields[text]" class="regular-text"
-value="<?php echo $meta['text']; ?>" >
-</p><br><br>
-					<a href="#">commentaire</a>
-				<p>
-<label for="your_fields[textarea]"></label>
-<br>
-<textarea name="your_fields[textarea]" id="your_fields[textarea]" rows="5" cols="30"
-style="width:500px;"><?php echo $meta['textarea'];?></textarea>
-</p>
-				</div>
-					<div class="clearfix"> </div>
-			</div>
-				<div class="billgates">
-				<div class="col-md-10 billgates1">
-					<h2></h2>
+				
+			
+				
 					<h5><a href="#">Samedi ,</a> 27 mais 2018 ,<a href="#">maisson de loaction des voitures</a></h5>
 				</div>
 				<div class="col-md-1 billgates2">
 					<h3>merci pour votre visite</h3>
-					<p><a href="#"></a></p>
+					
 				</div>
+				<div class="loadmore">
+			<a class="more" href="single.html">lire plus</a>
+	</div>
 					<div class="clearfix"> </div>
 					<h4>“crée par”</h4>
 					<h6>jlassi haifa<br>naffouti rabeb</br></h6>
 				</div>
 	</div>
-	<div class="loadmore">
-			<a class="more" href="single.html">lire plus</a>
-	</div>
-	<div class="loadmore1">
-			<a class="load" href="#">voir plus</a>	
-	</div>
+	
+	
 </div>	
 </div>
 <!-- content -->
--- header.php
+++ header.php
@@ -7,16 +7,31 @@
 			<div class="logo">
 				<a href="index.html"><img src="images/logo.png" class="img-responsive" alt="" /></a>
 			</div>
+			<div class="lequotes">
+		
+			<div class="col-md-12">
+				<a class="more" href="single.html"> careterstique de voiture</a>
+				<a class="more" href="single.html"> type de voiture</a>
+				<a class="more" href="single.html"> acceuile</a>
+				<a class="more" href="single.html"> liux de socite de location</a></div>
+				<div class="search-box">
+				        <form>
+				            <input type="text" name="s" class="textbox" value="search something" onFocus="this.value = '';" onBlur="if
+				                  	(this.value == '') {this.value = 'search something';}">
+				            <input type="submit" value>
+				        </form>
+		            </div>
+			</div>
 		</div> 
 			<div class="header-bottom">
 				<div class="container">
 					<div class="head-nav">
 						<span class="menu"> </span>
 							<ul>
-								<li class="active"><a href="index.html">Home</a></li>
+								<!--<li class="active"><a href="index.html">Home</a></li>
 								<li><a href="about.html">About</a></li>
 								<li><a href="index.html">Categories</a></li>
-								<div class="clearfix"> </div>
+								<div class="clearfix"> </div>-->
 						</ul>
 				</div>
 				<!-- script-for-nav -->
@@ -28,13 +43,7 @@
 						});
 					</script>
 				<!-- script-for-nav -->		
-					<div class="search-box">
-				        <form>
-				            <input type="text" name="s" class="textbox" value="search something" onFocus="this.value = '';" onBlur="if
-				                  	(this.value == '') {this.value = 'search something';}">
-				            <input type="submit" value>
-				        </form>
-		            </div>
+					
 						<div class="clearfix"> </div>
 				</div>
 			</div>
